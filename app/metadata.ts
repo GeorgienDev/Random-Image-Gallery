1f06f3c4d49f5c9bc09486dfe03ded2b08aecde6
GeorgienDev <GeorgienDev@users.noreply.github.com>
2025-11-14 01:55:21 +0400

    Main commit

---
+++ app/metadata.ts
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Random Image Gallery",
+  description: "Random Image Gallery",
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Gallery from "./gallery/gallery";
 
 export default function Home() {
   return (
   <>
-  
+  <Gallery/>
   </>
   );
 }
