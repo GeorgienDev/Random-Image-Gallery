febdbf1a755255158bf327fdbb4227c98976858e
GeorgienDev <GeorgienDev@users.noreply.github.com>
2025-11-14 15:04:17 +0400

    fixed css

--- out/_next/static/41dcaY9h1tvQhWOmeyw1V/_buildManifest.js
+++ out/_next/static/41dcaY9h1tvQhWOmeyw1V/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Random-Image-Gallery//_next/:path+",
+        "destination": "/Random-Image-Gallery/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
